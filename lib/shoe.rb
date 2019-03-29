0d192e8596e36f798844e311103e9addde058891
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-03-29 04:07:12 +0000

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,13 +2,29 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    BRANDS.push(brand) unless BRANDS.include?(brand)
   end
 
   def cobble
     self.condition = "new"
     puts "Your shoe is as good as new!"
+  end
+
+  def brand=(brand)
+    @brand = brand
+    count = 0
+    while count <= BRANDS.length
+      BRANDS.each {|name|
+        if name != brand
+          BRANDS << brand
+        end}
+        count += 1
+
+    end
   end
 
 end
